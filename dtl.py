9fbba9dfcdf3c660fa6b1ea64c7d952a0285498a
PencilShavings <eb.pencilshavings@protonmail.com>
2024-03-15 12:32:19 -0700

    Refactored vars & functions

--- dtl.py
+++ dtl.py
@@ -23,31 +23,6 @@ def _get_old_path(library, project):
 
     return old_path
 
-
-def _rename_base_path(library, old, new):
-    """Renames the last location.
-    Taken from move_darktable_photos.py"""
-
-    library_path = os.path.expanduser(library)
-    new_dir = re.sub(r'/$', '', os.path.expanduser(new))
-    old_dir = re.sub(r'/$', '', os.path.expanduser(old))
-
-    conn = sqlite3.connect(library_path)
-    cur = conn.cursor()
-    ret = cur.execute("""select id, folder from film_rolls""")
-
-    cnt = 0
-    for elem in ret.fetchall():
-        new_folder = elem[1].replace(old_dir, new_dir)
-        cur.execute("""update film_rolls set folder='%s' where id='%s'""" %
-                    (new_folder, elem[0]))
-        cnt += 1
-
-    conn.commit()
-
-    click.echo("Renamed %s film rolls from %s to %s" % (cnt, old_dir, new_dir))
-
-
 def _launch_dt(project_path, use_flatpak):
     """Launch Darktable"""
 
@@ -72,7 +47,10 @@ def _launch_dt(project_path, use_flatpak):
         subprocess.call('darktable ' + cmd_options, shell=True)
 
 
-def path_processor(path):
+def _format_path(path):
+    """Formats the targeted path.
+        Expands a relative path to an absolute path while also removing any trailing slash."""
+
     # If path ends with a `/`. If so remove it.
     if str(path).endswith('/'):
         path = path.rstrip('/')
@@ -82,6 +60,29 @@ def path_processor(path):
 
     return path
 
+def _update_base_path(library, old_path, new_path):
+    """Renames the last location.
+    Taken from move_darktable_photos.py"""
+
+    library_path = os.path.expanduser(library)
+    new_dir = re.sub(r'/$', '', os.path.expanduser(new_path))
+    old_dir = re.sub(r'/$', '', os.path.expanduser(old_path))
+
+    conn = sqlite3.connect(library_path)
+    cur = conn.cursor()
+    ret = cur.execute("""select id, folder from film_rolls""")
+
+    cnt = 0
+    for elem in ret.fetchall():
+        new_folder = elem[1].replace(old_dir, new_dir)
+        cur.execute("""update film_rolls set folder='%s' where id='%s'""" %
+                    (new_folder, elem[0]))
+        cnt += 1
+
+    conn.commit()
+
+    click.echo("Renamed %s film rolls from %s to %s" % (cnt, old_dir, new_dir))
+
 
 @click.group()
 @click.version_option(version=__version__, message=__version__)
@@ -89,39 +90,12 @@ def cli():
     """A cli for creating and opening multiple Darktable libraries."""
     pass
 
-
-@cli.command('open')
-@click.argument('project_path')
-@click.option('--use-flatpak', is_flag=True, help="Use the Darktable flatpak.")
-def open_project(project_path, use_flatpak):
-    """Open an existing project."""
-
-    project_path = path_processor(project_path)
-    project_name = project_path.split('/')[-1]
-    library = project_path + '/.darktable/library.db'
-
-    if not osutil.dir_exists(project_path):
-        click.echo('"' + project_path + '" does not exist!')
-    else:
-        print(library)
-        if osutil.file_exists(library):
-            old_path = _get_old_path(library, project_name)
-
-            if project_path != old_path:
-                click.echo('Updating location')
-                _rename_base_path(library, old_path, project_path)
-            else:
-                click.echo('Locations match')
-
-    _launch_dt(project_path, use_flatpak)
-
-
 @cli.command('create')
 @click.argument('project_path')
 def new_project(project_path):
     """Create a new project."""
 
-    project_path = path_processor(project_path)
+    project_path = _format_path(project_path)
 
     if not osutil.dir_exists(project_path):
         click.echo('Creating: ' + project_path)
@@ -134,6 +108,31 @@ def new_project(project_path):
     else:
         print('The directory "' + project_path + '" already exists!')
         click.echo('The directory "' + project_path + '" already exists!')
+
+@cli.command('open')
+@click.argument('project_path')
+@click.option('--use-flatpak', is_flag=True, help="Use the Darktable flatpak.")
+def open_project(project_path, use_flatpak):
+    """Open an existing project."""
+
+    project_path = _format_path(project_path)
+    project_name = project_path.split('/')[-1]
+    library = project_path + '/.darktable/library.db'
+
+    if not osutil.dir_exists(project_path):
+        click.echo('"' + project_path + '" does not exist!')
+    else:
+        print(library)
+        if osutil.file_exists(library):
+            old_path = _get_old_path(library, project_name)
+
+            if project_path != old_path:
+                click.echo('Updating location')
+                _update_base_path(library, old_path, project_path)
+            else:
+                click.echo('Locations match')
+
+    _launch_dt(project_path, use_flatpak)
 
 
 if __name__ == '__main__':
